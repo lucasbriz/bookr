443b2f7b669a2bee5cfb74958035029b91e5cd0a
Lucas Briz <lucasbriz@outlook.com>
2024-11-11 15:07:00 -0300

    initial commit

---
+++ src/test/java/com/lucorp/bookr/BookrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucorp.bookr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
